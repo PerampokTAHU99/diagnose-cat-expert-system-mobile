bfd51f02f8e3b47cb4bdeaeb15c92dc40b758484
Mameng57 <takiya2888157@gmail.com>
2023-09-08 22:02:21 +0700

    tambah ruting system baru (named routes)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
-import 'package:cat/screens/splash.dart';
+import 'package:cat/screens/diagnosa_hasil.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'screens/daftar.dart';
+import 'screens/diagnosa.dart';
+import 'screens/home.dart';
+import 'screens/riwayat.dart';
+import 'screens/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,10 +24,44 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Poppins',
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: const Color(0xffEAC696),
-          secondary: const Color(0xff967758)
+          secondary: const Color(0xff967758),
         ),
       ),
-      home: const Splash(),
+      initialRoute: "/",
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case "/":
+            return MaterialPageRoute(builder: ((context) => const Splash()));
+          case "/home":
+            return PageTransition(
+              child: const Home(),
+              type: PageTransitionType.fade,
+            );
+          case "/diagnosa":
+            return PageTransition(
+              child: const Diagnosa(),
+              type: PageTransitionType.leftToRightWithFade,
+            );
+          case "/diagnosa-hasil":
+            return PageTransition(
+              child: const DiagnosaHasil(),
+              type: PageTransitionType.bottomToTop,
+            );
+          case "/riwayat":
+            return PageTransition(
+              child: const Riwayat(),
+              type: PageTransitionType.bottomToTop,
+            );
+          case "/daftar":
+          return PageTransition(
+            child: const Daftar(),
+            type: PageTransitionType.rightToLeftWithFade,
+          );
+          default: return MaterialPageRoute(
+            builder: ((context) => const Placeholder()),
+          );
+        }
+      },
     );
   }
 }
